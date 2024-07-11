87fee52440561e6c2006acbb258a590d70bd4ba6
Gurjit Singh <gurjit9320@gmail.com>
2024-07-11 13:12:16 +0530

    fixed hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -66,7 +66,7 @@ const Hero = () => {
                   <Image src={quotation} w={60} alt="Priyanka Chopra" />
 
                   <div className=" text-slate-700  font-normal font-['FONTSPRING DEMO - Argent CF'] leading-normal">
-                    It’s the best thing you’ll give yourself because you’re
+                    It's the best thing you'll give yourself because you're
                     taking the power from your biological clock, and you can
                     work until however long you want. Your eggs will still be
                     the same age as when you froze them.
