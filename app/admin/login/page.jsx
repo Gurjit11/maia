908aca12e9f7e63871f3fd4a22dde0d71da8be86
Gurjit Singh <gurjit9320@gmail.com>
2024-09-28 18:31:50 +0530

    admin login

--- app/admin/login/page.jsx
+++ app/admin/login/page.jsx
@@ -1,114 +1,123 @@
 import Image from "next/image";
 import adminlogin from "../../../public/adminlogin.png";
+import icon from "../../../public/icon.png";
+import {
+  AiFillFacebook,
+  AiFillGoogleCircle,
+  AiOutlineFullscreenExit,
+} from "react-icons/ai";
+import { FaDoorOpen } from "react-icons/fa";
 
 const AdminLogin = () => {
   return (
-    <div className="grid grid-cols-2 ">
-      <div className="col-span-1 flex justify-center items-center">
-        <div className=" flex-col justify-start items-start gap-8 flex">
-          <div className="self-stretch h-36 flex-col justify-start items-center gap-6 flex">
-            <div className="justify-start items-center gap-1 inline-flex">
-              <div className="w-12 h-8 relative"></div>
-              <div className="text-red-400 text-2xl font-normal font-['Poppins'] leading-7">
-                MAIA.CARE
-              </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
+      <div className="col-span-1 flex justify-center items-center px-4">
+        <div className="flex flex-col justify-start items-start gap-2 w-full max-w-md">
+          <div className="self-stretch  flex-col justify-start items-center  flex">
+            <div className="justify-center w-full items-center gap-2 flex">
+              <Image src={icon} alt="logo" height={80} />
             </div>
-            <div className="self-stretch h-20 flex-col justify-start items-center gap-2 flex">
-              <div className="self-stretch text-center text-slate-800 text-4xl font-semibold font-['Poppins'] leading-10">
+            <div className=" flex-col justify-start items-center gap-2 flex">
+              <div className="self-stretch text-center text-slate-800 text-3xl font-semibold font-['Poppins'] leading-10">
                 Sign In To Your Account.
               </div>
-              <div className="self-stretch text-center text-slate-600 text-lg font-normal font-['Poppins'] leading-7">
+              <div className="self-stretch text-sm text-center text-slate-600 text-lg font-normal font-['Poppins'] leading-7">
                 Please enter details to access your dashboard
               </div>
             </div>
           </div>
-          <div className="self-stretch h-80 flex-col justify-start items-center gap-6 flex">
-            <div className="self-stretch h-52 flex-col justify-end items-start gap-6 flex">
-              <div className="self-stretch h-20 flex-col justify-start items-end gap-2 flex">
-                <div className="self-stretch h-20 flex-col justify-start items-end gap-2 flex">
-                  <div className="self-stretch text-slate-800 text-sm font-semibold font-['Poppins'] leading-tight">
-                    Email Address
-                  </div>
-                  <div className="self-stretch p-3 bg-white rounded-lg border border-slate-300 justify-start items-center gap-3 inline-flex">
-                    <div className="grow shrink basis-0 h-5 justify-start items-center gap-2 flex">
-                      <div className="w-5 h-5 px-0.5 py-1 justify-center items-center flex" />
-                      <div className="grow shrink basis-0 text-slate-600 text-base font-medium font-['Poppins'] leading-snug">
-                        Admin@maiacare.com
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="self-stretch h-28 flex-col justify-end items-start gap-3 flex">
-                <div className="self-stretch h-20 flex-col justify-start items-end gap-2 flex">
-                  <div className="self-stretch h-20 flex-col justify-start items-end gap-2 flex">
-                    <div className="self-stretch text-slate-800 text-sm font-semibold font-['Poppins'] leading-tight">
-                      Password
-                    </div>
-                    <div className="self-stretch p-3 bg-white rounded-lg border border-slate-300 justify-start items-center gap-3 inline-flex">
-                      <div className="grow shrink basis-0 h-5 justify-start items-center gap-2 flex">
-                        <div className="w-5 h-5 px-0.5 pt-px pb-0.5 justify-center items-center flex" />
-                        <div className="grow shrink basis-0 text-slate-600 text-base font-medium font-['Poppins'] leading-snug">
-                          *****************
-                        </div>
-                      </div>
-                      <div className="w-5 h-5 px-px py-0.5 justify-center items-center flex" />
-                    </div>
-                  </div>
-                </div>
-                <div className="self-stretch justify-end items-center gap-4 inline-flex">
-                  <div className="justify-center items-center gap-2 flex">
-                    <div className="text-slate-700 text-sm font-semibold font-['Poppins'] underline leading-tight">
-                      Forgot Password
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div className="self-stretch flex-col justify-start items-center gap-3 flex">
+            {/* Email Input */}
+            <div className="w-full">
+              <label
+                htmlFor="email"
+                className="block text-slate-800 font-semibold font-['Poppins'] leading-tight"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full p-3 bg-white rounded-lg border border-slate-300 text-base font-medium font-['Poppins'] leading-snug"
+                placeholder="Admin@maiacare.com"
+              />
             </div>
-            <div className="self-stretch h-24 flex-col justify-start items-center gap-6 flex">
-              <div className="self-stretch px-5 py-3 bg-slate-700 rounded-lg justify-center items-center gap-2.5 inline-flex">
-                <div className="text-white text-base font-semibold font-['Poppins'] leading-snug">
-                  Sign In
-                </div>
-                <div className="w-5 h-5 px-0.5 py-0.5 justify-center items-center flex" />
-              </div>
-              <div className="self-stretch justify-center items-center gap-1 inline-flex">
-                <div className="text-slate-800 text-sm font-normal font-['Poppins'] leading-tight">
-                  Don’t have an account?
-                </div>
-                <div className="justify-center items-center gap-2 flex">
-                  <div className="text-slate-700 text-sm font-semibold font-['Poppins'] underline leading-tight">
-                    Sign Up
-                  </div>
-                </div>
-              </div>
+
+            {/* Password Input */}
+            <div className="w-full">
+              <label
+                htmlFor="password"
+                className="block text-slate-800  font-semibold font-['Poppins'] leading-tight"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full p-3 bg-white rounded-lg border border-slate-300 text-base font-medium font-['Poppins'] leading-snug"
+                placeholder="*****************"
+              />
+            </div>
+
+            {/* Forgot Password */}
+            <div className="self-stretch flex justify-end items-center">
+              <a
+                href="#"
+                className="text-slate-700 text-sm font-semibold font-['Poppins'] underline leading-tight"
+              >
+                Forgot Password
+              </a>
             </div>
           </div>
-          <div className="self-stretch justify-start items-center gap-3 inline-flex">
-            <div className="text-center text-slate-400 text-xs font-semibold font-['Poppins'] uppercase leading-none tracking-wide">
+
+          {/* Sign In Button */}
+          <div className="self-stretch flex flex-col gap-6">
+            <button className="w-full px-5 py-3 bg-slate-700 rounded-lg text-white text-base font-semibold font-['Poppins']">
+              Sign In <FaDoorOpen className="text-xl inline" />
+            </button>
+            <div className="self-stretch text-center text-slate-800 text-sm font-normal font-['Poppins'] leading-tight">
+              Don't have an account?{" "}
+              <a
+                href="#"
+                className="text-slate-700 text-sm font-semibold font-['Poppins'] underline"
+              >
+                Sign Up
+              </a>
+            </div>
+          </div>
+
+          {/* OR Section */}
+          <div className="self-stretch justify-center items-center gap-3 flex">
+            <div className="text-center text-slate-400 text-xs font-semibold font-['Poppins'] uppercase tracking-wide">
               OR
             </div>
           </div>
-          <div className="self-stretch h-24 flex-col justify-start items-end gap-2 flex">
-            <div className="self-stretch px-4 py-2.5 bg-white rounded-lg border border-slate-300 justify-center items-center gap-3 inline-flex">
-              <div className="w-6 h-6 relative" />
-              <div className="text-slate-800 text-base font-medium font-['Poppins'] leading-snug">
+
+          {/* Social Sign-In Buttons */}
+          <div className="self-stretch flex-col justify-start items-center ">
+            <button className="w-full mb-3 px-4 py-2.5 bg-white rounded-lg border border-slate-300 flex justify-center items-center gap-3">
+              <AiFillFacebook className="text-2xl" />
+              <span className="text-slate-800 text-base font-medium font-['Poppins']">
                 Sign In With Facebook
-              </div>
-            </div>
-            <div className="self-stretch px-4 py-2.5 bg-white rounded-lg border border-slate-300 justify-center items-center gap-3 inline-flex">
-              <div className="w-6 h-6 pr-px justify-center items-center flex">
-                <div className="w-6 h-6 relative"></div>
-              </div>
-              <div className="text-slate-800 text-base font-medium font-['Poppins'] leading-snug">
+              </span>
+            </button>
+            <button className="w-full px-4 py-2.5 bg-white rounded-lg border border-slate-300 flex justify-center items-center gap-3">
+              <AiFillGoogleCircle className="text-2xl" />
+              <span className="text-slate-800 text-base font-medium font-['Poppins']">
                 Sign In With Google
-              </div>
-            </div>
+              </span>
+            </button>
           </div>
         </div>
       </div>
-      <div className="col-span-1 ">
-        <Image src={adminlogin} className="h-screen w-auto" />
+
+      {/* Image Section */}
+      <div className="hidden md:block col-span-1">
+        <Image
+          src={adminlogin}
+          alt="Admin Login Image"
+          className="h-screen w-auto object-cover"
+        />
       </div>
     </div>
   );
