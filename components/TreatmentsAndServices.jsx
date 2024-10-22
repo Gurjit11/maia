ea2776fc01d74f4642716b7c03d764a87209fa9d
Gurjit Singh <gurjit9320@gmail.com>
2024-10-22 15:38:16 +0530

    animated treatments and services

--- components/TreatmentsAndServices.jsx
+++ components/TreatmentsAndServices.jsx
@@ -10,6 +10,9 @@ import ivf from "../public/ivf.png";
 import iuitreatment from "../public/iuitreatment.png";
 import ivfservices from "../public/ivfservices.png";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 
 const treatments = {
   naturalFertilitySupport: {
@@ -23,40 +26,40 @@ const treatments = {
         title: "Nutritional Counseling",
         description:
           "Personalized diet plans designed to optimize fertility and overall health.",
-          image: iuitreatment,
-        },
+        image: iuitreatment,
+      },
       {
         title: "Herbal Supplements",
         description:
           "Utilization of specific herbs to balance hormones and support reproductive function.",
-          image: iuitreatment,
-        },
+        image: iuitreatment,
+      },
       {
         title: "Acupuncture",
         description:
           "A therapy that helps improve blood flow to reproductive organs and reduce stress levels.",
-          image: iuitreatment,
-        },
+        image: iuitreatment,
+      },
       {
         title: "Stress Management",
         description:
           "Techniques such as yoga and meditation to alleviate stress that may affect fertility.",
-          image: iuitreatment,
-        },
+        image: iuitreatment,
+      },
       {
         title: "Fertility Tracking",
         description:
           "Monitoring ovulation and menstrual cycles to identify the best times for conception.",
-          image: iuitreatment,
-        },
+        image: iuitreatment,
+      },
       {
         title: "Lifestyle Coaching",
         description:
           "Guidance on exercise, sleep, and minimizing exposure to environmental toxins to improve fertility.",
-          image: iuitreatment,
-        },
+        image: iuitreatment,
+      },
     ],
-},
+  },
 
   ivf: {
     image: ivf,
@@ -176,7 +179,7 @@ const treatments = {
         image: iuitreatment,
       },
     ],
-},
+  },
 
   // Add other treatment types if needed like IUI, Egg & Sperm Freezing, etc.
 };
@@ -189,63 +192,85 @@ const TreatmentsAndServices = () => {
     setExpandedIndex(expandedIndex === index ? null : index);
   };
 
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, threshold: 0.6 });
+
   return (
     <div className="flex justify-center items-center">
-      <div className="flex-col justify-center w-full  items-center max-w-5xl py-10 p-2 sm:p-5 ">
-        <div className="flex-col mb-10 justify-center items-center ">
+      <div className="flex-col justify-center w-full items-center max-w-5xl py-10 p-2 sm:p-5">
+        {/* Header */}
+        <motion.div
+          className="flex-col mb-10 justify-center items-center"
+          initial={{ opacity: 0, y: 50 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.8 }}
+          ref={ref}
+        >
           <div className="text-[#2B4360] text-center flex justify-center items-center font-bold text-2xl sm:text-3xl">
             Treatments and Services
           </div>
           <div className="text-gray-500 flex justify-center items-center">
             PROCESS
           </div>
-        </div>
+        </motion.div>
+
         {/* Tabs */}
-        <div className="flex sm:w-full text-nowrap overflow-x-auto justify-around space-x-8 border-b-2">
+        <motion.div
+          className="flex sm:w-full text-nowrap overflow-x-auto justify-around space-x-8 border-b-2"
+          initial={{ opacity: 0, y: 50 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
           <button
             className={`pb-2 ml-4 text-lg font-semibold ${
               activeTab === "naturalFertilitySupport"
                 ? "border-b-2 border-[#2B4360] text-[#2B4360]"
-                : "text-gray-400 "
+                : "text-gray-400"
             }`}
             onClick={() => setActiveTab("naturalFertilitySupport")}
           >
             Fertility Support
           </button>
           <button
-            className={`pb-2  text-lg font-semibold ${
+            className={`pb-2 text-lg font-semibold ${
               activeTab === "ivf"
                 ? "border-b-2 border-[#2B4360] text-[#2B4360]"
-                : "text-gray-400 "
+                : "text-gray-400"
             }`}
             onClick={() => setActiveTab("ivf")}
           >
             IVF Treatment
           </button>
           <button
-            className={`pb-2  text-lg font-semibold ${
+            className={`pb-2 text-lg font-semibold ${
               activeTab === "iui"
                 ? "border-b-2 border-[#2B4360] text-[#2B4360]"
-                : "text-gray-400 "
+                : "text-gray-400"
             }`}
             onClick={() => setActiveTab("iui")}
           >
             IUI Treatment
           </button>
           <button
-            className={`pb-2  text-lg font-semibold ${
+            className={`pb-2 text-lg font-semibold ${
               activeTab === "eggAndSpermFreezing"
                 ? "border-b-2 border-[#2B4360] text-[#2B4360]"
-                : "text-gray-400 "
+                : "text-gray-400"
             }`}
             onClick={() => setActiveTab("eggAndSpermFreezing")}
           >
             Egg & Sperm Freezing
           </button>
-        </div>
+        </motion.div>
 
         {/* Content */}
-        <div className="mb-16 mt-6">
+        <motion.div
+          className="mb-16 mt-6"
+          key={activeTab}
+          initial={{ opacity: 0, y: 50 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.8, delay: 0.4 }}
+        >
           <div className="flex">
             <div className="pr-2 sm:pr-4">
               <Image
@@ -263,20 +288,24 @@ const TreatmentsAndServices = () => {
               </p>
             </div>
           </div>
+
           <div className="sm:grid grid-cols-4 gap-10">
             {/* Cards */}
             <div className="col-span-3 sm:grid h-min grid-cols-2 gap-4">
               {treatments[activeTab]?.faq.map((treatment, index) => (
-                <div
+                <motion.div
                   key={index}
-                  className=" border h-min rounded-lg my-2 sm:my-0 shadow-sm bg-[#e6f3f2]"
+                  className="border h-min rounded-lg my-2 sm:my-0 shadow-sm bg-[#e6f3f2]"
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={isInView ? { opacity: 1, y: 0 } : {}}
+                  transition={{ duration: 0.6, delay: index * 0.2 + 0.4 }}
                 >
                   <div
                     className="flex justify-between items-center cursor-pointer"
                     onClick={() => handleExpand(index)}
                   >
-                    <div className=" flex font-semibold text-[#2B4360]">
-                      <Image className="h-10" src={treatment.image} alt={""} />
+                    <div className="flex font-semibold text-[#2B4360]">
+                      <Image className="h-10" src={treatment.image} alt="" />
                       <span className="p-2 text-sm sm:text-base">
                         {treatment.title}
                       </span>
@@ -289,22 +318,31 @@ const TreatmentsAndServices = () => {
                       )}
                     </button>
                   </div>
-                  {expandedIndex === index && (
-                    <p className="mt-2 p-2 text-sm sm:text-base text-gray-600">
-                      {treatment.description}
-                    </p>
-                  )}
-                </div>
+                  {/* Animate expanding and collapsing content */}
+                  <AnimatePresence>
+                    {expandedIndex === index && (
+                      <motion.div
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: "auto", opacity: 1 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{ duration: 0.3 }}
+                      >
+                        <p className="mt-2 p-2 text-sm sm:text-base text-gray-600">
+                          {treatment.description}
+                        </p>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </motion.div>
               ))}
             </div>
             <div className="col-span-1 hidden sm:block">
               <Image src={treatments[activeTab]?.image2} alt="IVF Services" />
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
 };
-
 export default TreatmentsAndServices;
