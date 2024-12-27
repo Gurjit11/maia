3478ad5eae0e997ec7393525cd26557f8f962857
Gurjit Singh <gurjit9320@gmail.com>
2024-12-27 13:42:45 +0530

    refactor: improve formatting and readability in DoctorProfile and ReviewList components

--- app/admin/reviews/page.jsx
+++ app/admin/reviews/page.jsx
@@ -5,8 +5,8 @@ import { useEffect, useState } from "react";
 
 const ReviewList = () => {
   const [reviews, setReviews] = useState([]);
-  const [sort, setSort] = useState("all")
-  const [ originalReviews,setOriginalReviews] = useState([]);
+  const [sort, setSort] = useState("all");
+  const [originalReviews, setOriginalReviews] = useState([]);
   const [search, setSearch] = useState("");
   const getReviews = async () => {
     let config = {
@@ -65,10 +65,38 @@ const ReviewList = () => {
     getReviews();
   }, []);
   console.log(reviews);
-  const toggleStatus = (index) => {
+  const toggleStatus = (reviewId, status) => {
+    let data = JSON.stringify({
+      reviewId: reviewId,
+      newStatus: status === "ACTIVE" ? "INACTIVE" : "ACTIVE",
+    });
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://maia.projectx38.cloud/maia-dashboard/reviews/updateStatus",
+      headers: {
+        "login-token":
+          "f48668d4ea1989d14a5692c5c4b7b296a2d651c4947526876dee65a7e191bacecdaff5bb1b21293df00fef97b96f1beb97ce695eb8ddb062ee48e912e5fddf83e7dc7008fcef956f29c78dae1f6486b433304398b040aa7f3312867c6d090ecbc5c5df4eaf8e21c8a0ecd3ac73b4469b59c892d51bf61fa9713f0cded76ded8a",
+        "Content-Type": "application/json",
+        Cookie:
+          "x38_gadighoda_admin_login_unique=f48668d4ea1989d14a5692c5c4b7b29629e8b2f6a9e870fbf0451038467110d0967eb7e0d9aec0714d2890d5e279f980412b02e37441a232867cbbed5478a25b6db22230c03d7b1d567a32edcd6355d48fe270e204b3aaa0113f3c6c3284696b649fd126ad6ce51dff09a20a2280ef49eaf875c81c8ebcc77337259757f19cc9; x38_gadighoda_admin_user_details=j%3A%7B%22_id%22%3A%226698d1db58e489d6943d9e30%22%2C%22userId%22%3A%22df2af83e-b7ff-4d70-b417-29f14701f3c9%22%2C%22userName%22%3A%22anikethandore12%40gmail.com%22%2C%22password%22%3A%22test1234%22%2C%22status%22%3A%22ACTIVE%22%2C%22name%22%3A%22Aniket%20Handore%22%7D",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
     setReviews((prevReviews) =>
-      prevReviews.map((review, i) =>
-        i === index
+      prevReviews.map((review) =>
+        review.reviewId === reviewId
           ? {
               ...review,
               status: review.status === "ACTIVE" ? "INACTIVE" : "ACTIVE",
@@ -78,14 +106,15 @@ const ReviewList = () => {
     );
   };
 
-
   useEffect(() => {
     if (sort === "all") {
       setReviews(originalReviews);
     } else {
       const sortedReviews = [...reviews].sort((a, b) => {
         if (sort === "recent") {
-          return new Date(b.createdAt.timestamp) - new Date(a.createdAt.timestamp);
+          return (
+            new Date(b.createdAt.timestamp) - new Date(a.createdAt.timestamp)
+          );
         }
         return 0;
       });
@@ -93,16 +122,16 @@ const ReviewList = () => {
     }
   }, [sort, originalReviews]);
 
-
-
-  useEffect(() => { 
+  useEffect(() => {
     if (search === "") {
       setReviews(originalReviews);
     } else {
       const filteredReviews = reviews.filter((review) =>
-        review?.reviewedBy?.name?.toLowerCase().includes(search?.toLowerCase() || "")
+        review?.reviewedBy?.name
+          ?.toLowerCase()
+          .includes(search?.toLowerCase() || "")
       );
-      setReviews(filteredReviews);  
+      setReviews(filteredReviews);
     }
   }, [search]);
   const handleEditClick = (customer) => {
@@ -110,7 +139,6 @@ const ReviewList = () => {
     setNewStatus(customer.status); // Pre-fill with current status
     setIsEditModalOpen(true);
   };
-
 
   return (
     <div className="p-6">
@@ -137,7 +165,8 @@ const ReviewList = () => {
         <div className="flex space-x-3 items-center">
           <div className="flex items-center space-x-1">
             <label className="text-gray-500">Sort by:</label>
-            <select className="border px-2 py-1 rounded-md" 
+            <select
+              className="border px-2 py-1 rounded-md"
               value={sort}
               onChange={(e) => setSort(e.target.value)}
             >
@@ -197,7 +226,9 @@ const ReviewList = () => {
                       type="checkbox"
                       className="sr-only peer"
                       checked={review?.status === "ACTIVE"}
-                      onChange={() => toggleStatus(index)}
+                      onChange={() =>
+                        toggleStatus(review.reviewId, review.status)
+                      }
                     />
                     <div className="w-11 h-6 bg-[#F1F5F9] rounded-full peer   dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#70AAA4]">
                       {/* <div className="absolute inset-0 flex items-center justify-center text-xs">
